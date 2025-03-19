50113bac99c11ebdce5fa0ede7a7586600301e13
Iryna Notkina <168377178+IrynaNotkina@users.noreply.github.com>
2025-03-19 22:06:26 +0100

    Add files via upload

---
+++ homework_4.1/js/main.js
@@ -0,0 +1,7 @@
+let userName = prompt ('Please enter your name');
+if (userName !== null) {
+    alert (`Hellow ${userName}, how are you?`);
+} else { alert ('You canceled the entry')};
+console.log(userName);
+
+
