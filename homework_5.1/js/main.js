df3ebf93378e1e8bf7e0689084561b363ed0bb36
Iryna Notkina <168377178+IrynaNotkina@users.noreply.github.com>
2025-03-20 02:35:57 +0100

    Add files via upload

---
+++ homework_5.1/js/main.js
@@ -0,0 +1,15 @@
+// let output = '';
+// for (let i = 20; i <= 30; i += 0.5) {
+//   output += i + ' ';
+// }
+// console.log(output);
+
+let i = 20;
+let output = '';
+
+while (i <=30) {
+    output += i + ' ';
+    i += 0.5;
+}
+
+console.log(output); 
